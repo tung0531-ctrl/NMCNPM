94886fc6b90716246688ca3ba4e43c1ba8d90261
Tung <tung3152005@gmail.com>
2025-11-30 23:16:56 +0700

    add signup_fe to test

---
+++ frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,23 @@
+import {create} from 'zustand'
+import {toast} from 'sonner'
+import type { AuthState } from '@/types/store';
+
+export const useAuthStore = create<AuthState>((set, get) => ({
+    accessToken: null,
+    user: null,
+    loading: false
+
+    signUp: async (username, password, email, firstName, lastName) => {
+        try {
+            set ({
+                loading: true
+            });
+            //gọi api
+            await authService.signUp(username, password, email, firstName, lastName);
+            toast.success('Đăng ký thành công! Bạn sẽ được chuyển sang trang đăng nhập');
+        } catch (error){
+            console.error(error);
+            toast.error('Đăng ký không thành công');
+        }
+    }
+}));
