a041d1609317a408f85814486487b496bb061e80
Tung <tung3152005@gmail.com>
2025-12-01 12:54:11 +0700

    signup fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// đang làm giống hệt theo ứng dụng ChatApp nên có một số đoạn không cần thiết phải xóa đi, thêm sửa
+
+import {BrowserRouter, Route, Routes} from 'react-router'
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ChatAppPage from './pages/ChatAppPage';
+import {Toaster} from 'sonner';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  return <>
+  <Toaster/>
+<BrowserRouter>
+<Routes>
+  {/* public routes*/}
+    <Route
+      path = '/signin'
+      element = {<SignInPage/>}
+      />
+  {/* protect routes*/}
+  {/* todo: tạo protected route */}
+    <Route
+      path = '/signup'
+      element = {<SignUpPage/>}
+      />
+    <Route
+      path = '/'
+      element = {<ChatAppPage/>}
+      />
+</Routes>
+</BrowserRouter>
+  </>
+};
 
 export default App
